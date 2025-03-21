f00adb32f4a28bc47aaa8bc8a00ab60daa1682f8
Sprocket Riggs <sprocket710@gmail.com>
2025-03-20 23:17:56 -0700

    More meson build stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
 int main() {
     std::cout << "Starting raster-rise!" << std::endl;
     return 0;
